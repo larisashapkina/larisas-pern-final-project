8e94f7abdca5615d70b6b9b7076642609a47a208
Larisa <larisashapkinab@gmail.com>
2022-02-23 21:50:11 -0500

    App is ready

--- front-end/src/Components/Footer.js
+++ front-end/src/Components/Footer.js
@@ -2,7 +2,6 @@ function Footer(){
     return(
         <div className="footer">
             <div className="footer-content">
-
                 <div className="footer-section about">
                     <h1 className="logo-text"><span>Stiletto</span> App</h1> 
                     <p>
@@ -13,24 +12,12 @@ function Footer(){
                         <span><i className="fas fa-phone"></i>☎️123-456-789</span>
                         <span><i className="fas fa-envelope"></i>📧 larisashapkina@pursuit.org</span>
                     </div>
-                    <div className="socials">
-                        <a href="#"><i className="fab fa-facebook"></i></a>
-                        <a href="#"><i className="fab fa-instagram"></i></a>
-                        <a href="#"><i className="fab fa-twitter"></i></a>
-                        <a href="#"><i className="fab fa-youtube"></i></a>
+                    <div className="footer-bottom">
+                        Designed by Larisa Shapkina
                     </div>
-            </div> 
-
-            
-                <div className="footer-section links"></div>
-                <div className="footer-section contact-form"></div>
-            
-
-                <div className="footer-bottom">
-                    Designed by Larisa Shapkina
                 </div>
             </div>
-         </div>   
+        </div>   
     )
 }
     
